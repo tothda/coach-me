a36477af31a05124d2bdc521f7ff80a263c4e1cf
Toth David <tothda@gmail.com>
2012-11-06 23:12:34 +0100

    Show distance on training#index
    Also fixing a bug of not doing authorization check on the actions of the
    trainings controller.

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,4 +1,6 @@
 class ExercisesController < ApplicationController
+  load_and_authorize_resource
+  
   # GET /exercises
   # GET /exercises.json
   def index
--- app/models/training.rb
+++ app/models/training.rb
@@ -2,4 +2,8 @@ class Training < ActiveRecord::Base
   has_many :exercises
   belongs_to :user
   attr_accessible :started_at, :title, :feeling, :temperature, :weather, :surface, :notes
+  
+  def total_distance
+    exercises.map(&:distance).map(&:to_f).sum
+  end
 end
