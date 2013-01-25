6344c9757af3142445c4459091252076cf706773
Toth David <tothda@gmail.com>
2013-01-25 20:58:43 +0000

    Feel like a star.

--- app/models/training.rb
+++ app/models/training.rb
@@ -2,7 +2,9 @@ class Training < ActiveRecord::Base
   has_many :exercises, :order => "id ASC"
   belongs_to :user
   attr_accessible :started_at, :title, :feeling, :temperature, :weather, :surface, :notes
-  
+
+  FEELINGS = [["Awesome", "awesome"],["Good", "good"],["So-so", "soso"],["Sluggish", "sluggish"],["Injured", "injured"]]
+  FEELING_VALUES =  {"awesome" => 5, "good" => 4, "soso" => 3, "sluggish" => 2, "injured" => 1}
   def total_distance
     exercises.map(&:distance).map(&:to_f).sum
   end
@@ -10,5 +12,9 @@ class Training < ActiveRecord::Base
   # if it has notes or at least one exercise
   def needs_summary?
     notes.present? || exercises.present?
+  end
+
+  def feeling_value
+    FEELING_VALUES[feeling] || 0
   end
 end
